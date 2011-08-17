46fe835149c5b8f3545fadc93c1ea7534c232ea4
jonathan.skeet <jonathan.skeet@f1531f90-9e50-11de-a36b-d11e950a0b04>
2011-08-17 19:39:54 +0000

    - Added NuGet package - Upgraded projects - Split solution file into VS2008 and VS2010 - Updated readme.txt - Applied Apache 2.0 licence everywhere

--- ConstraintChanger/Program.cs
+++ ConstraintChanger/Program.cs
@@ -8,25 +8,24 @@ namespace ConstraintChanger
     class Program
     {
         const string InputAssembly = "UnconstrainedMelody.dll";
-        const string OutputAssembly = @"..\..\..\Rewritten\UnconstrainedMelody.dll";
+        const string OutputAssembly = InputAssembly;
+        const string OutputDirectory = @"../../../Rewritten";
+
+        private static readonly string[] SdkPaths = 
+        {
+            @"Microsoft SDKs\Windows\v6.0A\bin",
+            @"Microsoft SDKs\Windows\v7.0A\bin",
+            @"Microsoft SDKs\Windows\v7\bin"
+        };
 
         static int Main()
         {
-            string programFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
-            string sdkDirectory = Path.Combine(programFiles, @"Microsoft SDKs\Windows\v6.0A\bin");
-            if (!Directory.Exists(sdkDirectory))
+            string ildasmExe = FindIldasm();
+            if (ildasmExe == null)
             {
-                Console.WriteLine("Can't find SDK. Aborting.");
-                return 1;
+                // Error message will already have been written
+                return 1;                 
             }
-
-            string ildasmExe = Path.Combine(sdkDirectory, "ildasm.exe");
-            if (!File.Exists(ildasmExe))
-            {
-                Console.WriteLine("Can't find ildasm. Aborting. Expected it at: {0}", ildasmExe);
-                return 1;
-            }
-
             string windows = Environment.GetFolderPath(Environment.SpecialFolder.System);
             string framework2Directory = Path.Combine(windows, @"..\Microsoft.NET\Framework\v2.0.50727");
             string ilasmExe = Path.Combine(framework2Directory, "ilasm.exe");
@@ -50,6 +49,25 @@ namespace ConstraintChanger
             return 0;
         }
 
+        private static string FindIldasm()
+        {
+            string programFiles = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+            foreach (string sdkPath in SdkPaths)
+            {
+                string directory = Path.Combine(programFiles, sdkPath);
+                if (Directory.Exists(directory))
+                {
+                    string ilasm = Path.Combine(directory, "ildasm.exe");
+                    if (File.Exists(ilasm))
+                    {
+                        return ilasm;
+                    }
+                }
+            }
+            Console.WriteLine("Unable to find SDK directory containing ilasm.exe. Aborting.");
+            return null;
+        }
+
         private static string Decompile(string ildasmExe)
         {
             string ilFile = Path.GetTempFileName();
@@ -66,10 +84,18 @@ namespace ConstraintChanger
 
         private static void Recompile(string ilFile, string ilasmExe)
         {
+            if (!Directory.Exists(OutputDirectory))
+            {
+                Console.WriteLine("Creating output directory");
+                Directory.CreateDirectory(OutputDirectory);
+            }
+
+            string output = Path.Combine(OutputDirectory, OutputAssembly);
+            Console.WriteLine("Recompiling {0} to {1}", ilFile, output);
             Process process = Process.Start(new ProcessStartInfo
             {
                 FileName = ilasmExe,
-                Arguments = "/OUTPUT=" + OutputAssembly + " /DLL " + "\"" + ilFile + "\"",
+                Arguments = "/OUTPUT=" + output + " /DLL " + "\"" + ilFile + "\"",
                 WindowStyle = ProcessWindowStyle.Hidden
             });
             process.WaitForExit();
