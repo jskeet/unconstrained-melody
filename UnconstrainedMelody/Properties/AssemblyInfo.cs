8c0b88c34976804fa2c772b47db0f985790f8034
Jon Skeet <skeet@pobox.com>
2015-03-29 16:53:59 +0100

    Fix URL and update the version number to 0.2.1.

--- UnconstrainedMelody/Properties/AssemblyInfo.cs
+++ UnconstrainedMelody/Properties/AssemblyInfo.cs
@@ -49,4 +49,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.2.0")]
+[assembly: AssemblyFileVersion("0.2.1.0")]
